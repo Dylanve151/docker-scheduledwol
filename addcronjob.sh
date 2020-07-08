2cb4798737a10e6f4e1caac8e52240cdc5fdbdc6
Dylanve151 <50217470+Dylanve151@users.noreply.github.com>
2020-07-08 19:13:29 +0200

    Update addcronjob.sh

--- addcronjob.sh
+++ addcronjob.sh
@@ -5,5 +5,5 @@ if [ -z "$CRONTIME" ]
 then
 	echo "No Crontime"
 else
-	echo "$CRONTIME root sh /root/wakeup.sh >/dev/null 2>&1" >> /etc/crontab
+	echo "$CRONTIME root /root/wakeup.sh >/dev/null 2>&1" >> /etc/crontab
 fi
