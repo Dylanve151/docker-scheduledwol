45ff1b1fb7d11ac9b5797ac25c49a965dc308139
Dylanve151 <50217470+Dylanve151@users.noreply.github.com>
2020-07-08 17:45:18 +0200

    Add files via upload

--- wakeup.sh
+++ wakeup.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 #WOL
 
-for MAC in $MAC_ADDRESS
-do  
-  wakeonlan -i $BROADCAST_IP $MAC
-done
+if [ -z "$TRIGGER_IP" ]
+then
+  for MAC in $MAC_ADDRESS
+  do  
+    wakeonlan -i $BROADCAST_IP $MAC
+  done
+else
+  until ping -c1 $TRIGGER_IP &>/dev/null; do :; done
+  for MAC in $MAC_ADDRESS
+  do  
+    wakeonlan -i $BROADCAST_IP $MAC
+  done
+fi
