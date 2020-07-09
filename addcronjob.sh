1e4141ba5e98f0322974496f43bb216637ab84f1
Dylanve151 <50217470+Dylanve151@users.noreply.github.com>
2020-07-09 22:14:49 +0200

    Update addcronjob.sh

--- addcronjob.sh
+++ addcronjob.sh
@@ -5,8 +5,8 @@ if [ -z "$CRONTIME" ]
 then
 	echo "No Crontime"
 else
-	echo "$CRONTIME root /root/wakeup.sh >>" > /etc/cron.d/wakeup
+	echo "$CRONTIME root /root/wakeup.sh >> log.log" > /etc/cron.d/wakeup
 	echo "" > /etc/cron.d/wakeup
 fi
 service cron start
-ping -i 1800 localhost
+tail -f /root/log.log
