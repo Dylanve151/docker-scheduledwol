b86d74518fe033fe1e9ac078178ba8189ca877db
Dylanve151 <50217470+Dylanve151@users.noreply.github.com>
2020-07-10 21:39:40 +0200

    Update addcronjob.sh

--- addcronjob.sh
+++ addcronjob.sh
@@ -8,5 +8,14 @@ else
 	echo "$CRONTIME root /root/wakeup.sh >> /root/log.log" > /etc/cron.d/wakeup
 	echo "" >> /etc/cron.d/wakeup
 fi
+echo "BROADCAST_IP=$BROADCAST_IP" > verb.sh
+echo "MAC_ADDRESS=$MAC_ADDRESS" >> verb.sh
+if [ -z "$TRIGGER_IP" ]
+then
+	echo "No Trigger IP"
+else
+	echo "TRIGGER_IP=$TRIGGER_IP" >> verb.sh
+fi
+chmod 755 verb.sh
 service cron start
 tail -f /root/log.log
