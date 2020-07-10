e2d632998f6730adda7aa122e4c84fbd6b0f4eb4
Dylanve151 <50217470+Dylanve151@users.noreply.github.com>
2020-07-10 21:59:47 +0200

    Update addcronjob.sh

--- addcronjob.sh
+++ addcronjob.sh
@@ -3,16 +3,15 @@
 
 if [ -z "$CRONTIME" ]
 then
-	echo "No Crontime"
+	echo "No Crontime" >> /root/log.log
 else
-	echo "$CRONTIME root /root/wakeup.sh >> /root/log.log" > /etc/cron.d/wakeup
-	echo "" >> /etc/cron.d/wakeup
+	echo "$CRONTIME root /root/wakeup.sh >> /root/log.log"$'\n' > /etc/cron.d/wakeup
 fi
 echo "$BROADCAST_IP" > BROADCAST_IP
 echo "$MAC_ADDRESS" > MAC_ADDRESS
 if [ -z "$TRIGGER_IP" ]
 then
-	echo "No Trigger IP"
+	echo "No Trigger IP" >> /root/log.log
 else
 	echo "$TRIGGER_IP" > TRIGGER_IP
 fi
