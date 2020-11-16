7dfd65e19bc7465285917ffe5b7d2ba9a7492782
Dylanve151 <50217470+Dylanve151@users.noreply.github.com>
2020-11-16 12:48:33 +0100

    Create startup.sh

---
+++ startup.sh
@@ -0,0 +1,10 @@
+echo "$BROADCAST_IP" > BROADCAST_IP
+echo "$MAC_ADDRESS" > MAC_ADDRESS
+if [ -z "$TRIGGER_IP" ]
+then
+	echo "No Trigger IP" >> log.log
+else
+	echo "$TRIGGER_IP" > TRIGGER_IP
+fi
+service cron start
+tail -f log.log
