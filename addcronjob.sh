750eede3764ea379839627e9841c44ccfdc1f53a
Dylanve151 <50217470+Dylanve151@users.noreply.github.com>
2020-11-16 12:48:57 +0100

    Update addcronjob.sh

--- addcronjob.sh
+++ addcronjob.sh
@@ -7,13 +7,3 @@ then
 else
 	echo "$CRONTIME root /root/wakeup.sh >> /root/log.log"$'\n' > /etc/cron.d/wakeup
 fi
-echo "$BROADCAST_IP" > BROADCAST_IP
-echo "$MAC_ADDRESS" > MAC_ADDRESS
-if [ -z "$TRIGGER_IP" ]
-then
-	echo "No Trigger IP" >> log.log
-else
-	echo "$TRIGGER_IP" > TRIGGER_IP
-fi
-service cron start
-tail -f log.log
